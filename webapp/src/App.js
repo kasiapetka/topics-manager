59d04c427000efad3b47655315bfb3b721ce53a1
kasiapetka <katarzynapetka7@gmail,com>
2020-03-02 23:06:59 +0100

    Index frontend changes.
    Navbar, jumbotron, background, icon.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TMNavbar from './TMNavbar';
+import TMJumbotron from './TMJumbotron';
 
 function App() {
   return (
     <div className="App">
       <TMNavbar />
+      <TMJumbotron/>
     </div>
   );
 }
---
+++ webapp/src/TMJumbotron.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Jumbotron, Button, Row, Col } from 'reactstrap';
+
+const TMJumbotron = (props) => {
+    return (
+        <div>
+            <Jumbotron className="jumbo jumbotron-fluid">
+                <Row>
+                    <Col sm="1"></Col>
+                    <Col sm="11">
+                        <h1 className="display-3">Task Manager</h1>
+                        <p className="lead">Simple way to keep all Your projects easily accessible, well-organised and consistent.</p>
+                        <hr className="my-2" />
+                        <p>Log in or create an account and enhance Your work with great project and groups management features.</p>
+                        <p className="lead">
+                            <Button color="secondary">Join Now</Button>
+                        </p>
+                    </Col>
+                </Row>
+            </Jumbotron>
+        </div>
+    );
+};
+
+export default TMJumbotron;
