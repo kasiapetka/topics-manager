ff66394c8d03c98b6bee2281ab62cdb2f39eb568
kasiapetka <katarzynapetka7@gmail,com>
2020-03-12 16:35:19 +0100

    Components as classes

--- src/main/webapp/src/components/LoginForm.js
+++ src/main/webapp/src/components/LoginForm.js
@@ -5,34 +5,41 @@ import {
     Button, Collapse,
 } from 'reactstrap';
 
-const LoginForm = (props) => {
-    return (
-        <div className="container-fluid w-50 border rounded pt-4 pb-5 mt-5">
-            <Form>
-                <h3 className="text-center">Sign In</h3>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-3">
-                    <Label for="exampleEmail" className="mr-sm-2">Email Address</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="Enter Email" />
-                </FormGroup>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-3">
-                    <Label for="examplePassword" className="mr-sm-2">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="Enter Password" />
-                </FormGroup>
-                <div className="form-row text-center pt-5">
-                    <div className="col-12">
-                        <Button type="submit" className="btn btn-primary w-25">Submit</Button>
-                    </div>
-                </div>
+export class LoginForm extends React.Component{
 
-                <div className="form-row text-center pt-3">
-                    <div className="col-12">
-                        <p>You don't have an account yet? Create new account here:</p>
-                        <Button type="submit" className="btn btn-primary w-25 mt-2">Sign Up</Button>
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container-fluid w-50 border rounded pt-4 pb-5 mt-5">
+                <Form>
+                    <h3 className="text-center">Sign In</h3>
+                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-3">
+                        <Label for="exampleEmail" className="mr-sm-2">Email Address</Label>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="Enter Email"/>
+                    </FormGroup>
+                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-3">
+                        <Label for="examplePassword" className="mr-sm-2">Password</Label>
+                        <Input type="password" name="password" id="examplePassword" placeholder="Enter Password"/>
+                    </FormGroup>
+                    <div className="form-row text-center pt-5">
+                        <div className="col-12">
+                            <Button type="submit" className="btn btn-primary w-25">Submit</Button>
+                        </div>
                     </div>
-                </div>
-            </Form>
-        </div>
-    );
+
+                    <div className="form-row text-center pt-3">
+                        <div className="col-12">
+                            <p>You don't have an account yet? Create new account here:</p>
+                            <Button type="submit" className="btn btn-primary w-25 mt-2">Sign Up</Button>
+                        </div>
+                    </div>
+                </Form>
+            </div>
+        );
+    }
 };
 
 export default LoginForm;
--- src/main/webapp/src/pages/LoginPage.js
+++ src/main/webapp/src/pages/LoginPage.js
@@ -3,12 +3,23 @@ import React from "react";
 import PageNavbar from "../components/PageNavbar";
 import LoginForm from "../components/LoginForm";
 
-const LoginPage =() => {
-    return (
-        <div>
-            <PageNavbar/>
-            <LoginForm/>
-        </div>
-    );
+class LoginPage extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogginActive: true
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <PageNavbar/>
+                <LoginForm/>
+            </div>
+        );
+    }
 };
+
 export default LoginPage;
