97913ae49e6b742761d31902bb38bbab73250dde
Matthew <matiklim.98@gmail.com>
2020-04-05 20:21:32 +0200

    Basic and simple teachers listing.

--- src/main/java/com/kasiapetka/topicsmanager/services/AdminService.java
+++ src/main/java/com/kasiapetka/topicsmanager/services/AdminService.java
@@ -1,7 +1,10 @@
 package com.kasiapetka.topicsmanager.services;
 
-import com.kasiapetka.topicsmanager.model.User;
+import com.kasiapetka.topicsmanager.model.Teacher;
+
+import java.util.List;
 
 public interface AdminService {
 //    void changePassword(User admin, String password);
+    List<Teacher> listTeachers();
 }
