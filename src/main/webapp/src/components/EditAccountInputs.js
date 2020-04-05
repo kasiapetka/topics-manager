50f91e415fe19f4369faaeab282ee2756002ec5e
kasiapetka <katarzynapetka7@gmail,com>
2020-04-05 17:24:54 +0200

    Working on acc modification

--- src/main/webapp/src/components/EditAccountInputs.js
+++ src/main/webapp/src/components/EditAccountInputs.js
@@ -4,14 +4,17 @@ import {Button, Form, FormGroup, Input, Label} from "reactstrap";
 
 const EditAccountInputs =(props)=> {
 
-    let nameDetails;
+    let nameDetails, invalidEmail, invalidPassword;
     const name = props.role === 'A' ? "Administrator" : props.person.name + " " + props.person.surname;
 
     nameDetails = (<FormGroup className="mr-sm-2  mt-2">
             <div color="light" id="name" className="col-12 pt-2 pb-2 pl-2 pr-2 border rounded text-center"
             >{name}</div>
         </FormGroup>
-    )
+    );
+
+    if (props.wrongEmail) invalidEmail = ' invalid';
+    if (props.wrongPassword) invalidPassword = ' invalid';
 
     return (
         <Form onSubmit={props.submit}>
@@ -27,7 +30,8 @@ const EditAccountInputs =(props)=> {
                 <Label for="exampleEmail" className="mr-sm-2 pl-1">New Email Address</Label>
                 <Input type="email" name="newEmail" id="exampleEmail" placeholder="Enter New Email"
                        value={props.person.newEmail || ''}
-                       onChange={props.change}/>
+                       onChange={props.change}
+                       {invalidEmail}/>
             </FormGroup>
             <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-3">
                 <Label for="newPassword" className="mr-sm-2 pl-1">New Password</Label>
@@ -43,7 +47,8 @@ const EditAccountInputs =(props)=> {
                     <Label for="examplePassword" className="mr-sm-2 pl-1">Confirm Changes Witch Password</Label>
                     <Input type="password" name="password" id="examplePassword" minLength="5"
                            placeholder="Your Password"
-                           value={props.person.password || ''} onChange={props.change}/>
+                           value={props.person.password || ''} onChange={props.change}
+                           {invalidPassword}/>
                 </FormGroup>
                 :
                 null}
